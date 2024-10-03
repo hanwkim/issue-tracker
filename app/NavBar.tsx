e334b7b72ee40dfc5de919b5a2d1441211eb23ec
Han Kim <hanwkim88@gmail.com>
2024-10-03 16:24:21 -0400

    Add styling for nav links, set up prisma and postgres

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath)
+
 	const links = [
 		{ label: "Dashboard", href: "/" },
 		{ label: "Issues", href: "/issues" },
@@ -17,7 +24,10 @@ const NavBar = () => {
 				{links.map((link) => (
 					<Link
 						key={link.href}
-						className="text-zinc-500 hover:text-zinc-800 transition-colors"
+						className={classNames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500 hover:text-zinc-800 transition-colors': link.href !== currentPath
+                        })}
 						href={link.href}
 					>
 						{link.label}
